5a2d5e814d5301dabf58bd6412efc05e90c1dbd7
crowe73 <rowechad73@yahoo.com>
2025-05-10 13:31:56 -0500

    Lab 1 First Program
    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-Hi
+print("Hi, My name is Chad Rowe.")
+print("People call me the \"Big Dog\".")
+print("I'm an aspiring game developer.")
+print("I'm presently developing a game called \n\"Space Defender\".")
